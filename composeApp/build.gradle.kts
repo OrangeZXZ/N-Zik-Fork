389006e885307766d547b7ed5e2349e77f0e2f1e
Daniels SOARES <danielo-24@hotmail.com>
2025-05-25 20:15:32 +0200

    added missing things

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -126,8 +126,8 @@ android {
         applicationId = "com.nevar.nzik"
         minSdk = 21
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.2.4"
+        versionCode = 7
+        versionName = "1.2.7"
 
         /*
                 UNIVERSAL VARIABLES
