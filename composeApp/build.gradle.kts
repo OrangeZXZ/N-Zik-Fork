e8de6889cfc53844cecaf28df0d4df6e6d856828
NΞVΛR <63956787+NEVARLeVrai@users.noreply.github.com>
2025-06-22 10:56:55 +0200

    TEST NO UPDATE

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -126,8 +126,8 @@ android {
         applicationId = "com.nevar.nzik"
         minSdk = 21
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.6.1"
+        versionCode = 14
+        versionName = "1.6.2"
 
         /*
                 UNIVERSAL VARIABLES
