2113e638f6fa675a9ac2ea48a04dacdf464d3b2d
NΞVΛR <danielo-24@hotmail.com>
2026-06-22 00:56:35 +0200

    bump ver 54

--- ComposeN-Zik/build.gradle.kts
+++ ComposeN-Zik/build.gradle.kts
@@ -114,8 +114,8 @@ android {
         applicationId = "com.nevar.nzik"
         minSdk = 24
         targetSdk = 37
-        versionCode = 53
-        versionName = "5.0.2"
+        versionCode = 54
+        versionName = "5.1.0"
 
         /*
                 UNIVERSAL VARIABLES
