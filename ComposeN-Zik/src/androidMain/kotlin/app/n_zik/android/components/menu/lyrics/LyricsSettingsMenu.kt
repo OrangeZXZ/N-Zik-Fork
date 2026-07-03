157830e9762bbc0d10435f3006bc11250795a7be
NΞVΛR <danielo-24@hotmail.com>
2026-07-03 13:58:04 +0200

    chore(lyrics): remove unused import

--- ComposeN-Zik/src/androidMain/kotlin/app/n_zik/android/components/menu/lyrics/LyricsSettingsMenu.kt
+++ ComposeN-Zik/src/androidMain/kotlin/app/n_zik/android/components/menu/lyrics/LyricsSettingsMenu.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
