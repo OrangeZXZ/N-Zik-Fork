04c3b2922f1a8d075acd08b5c679b56e747be3cf
NΞVΛR <danielo-24@hotmail.com>
2026-07-17 13:20:21 +0200

    update foss config

--- ComposeN-Zik/build.gradle.kts
+++ ComposeN-Zik/build.gradle.kts
@@ -180,8 +180,12 @@ android {
         }
 
         create( "foss" ) {
+            // App's properties
+            versionNameSuffix = "-foss"
+            applicationIdSuffix = ".foss"
             initWith( maybeCreate("full") )
             buildConfigField( "Boolean", "IS_AUTOUPDATE", "false" )
+            
             // Fallback for modules that don't have a 'foss' build type
             matchingFallbacks += listOf("release")
         }
