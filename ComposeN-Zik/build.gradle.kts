b3373e04f0d439d866d75b4d7da95e7a962cf857
NΞVΛR <danielo-24@hotmail.com>
2026-08-01 17:47:51 +0200

    bump 75 beta

--- ComposeN-Zik/build.gradle.kts
+++ ComposeN-Zik/build.gradle.kts
@@ -114,8 +114,8 @@ android {
         applicationId = "com.nevar.nzik"
         minSdk = 24
         targetSdk = 37
-        versionCode = 74
-        versionName = "7.2.9"
+        versionCode = 75
+        versionName = "7.3.1"
 
         /*
                 UNIVERSAL VARIABLES
